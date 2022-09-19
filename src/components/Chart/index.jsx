1a815746b577d13a6ce36e855dc9fb677750c1f6
Martyniuk Viktor <v.martyniuk@bvblogic.com>
2022-09-19 12:09:05 +0100

    Added some styles for chart

--- src/components/Chart/index.jsx
+++ src/components/Chart/index.jsx
@@ -40,6 +40,11 @@ export const options = {
             text: 'Revenue',
         },
     },
+    datasets: {
+        line: {
+            pointRadius: 0,
+        }
+    }
 };
 
 const PERIOD_OPTIONS = [
@@ -53,6 +58,8 @@ const DATASETS_OPTIONS = {
     label: "Revenue",
     borderColor: 'rgb(53, 162, 235)',
     backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    tension: 0.5,
+
 }
 
 export function BarChart({ rawData }) {
